b2bebe549e5a54307dbcc06b5cbfd7a3c159b701
amit suthar <amitmakar98602@gmail.com>
2025-08-29 16:38:44 +0530

    Added: different sql for inserting data

--- Backend/sql/1.1-roleTypes-insert.sql
+++ Backend/sql/1.1-roleTypes-insert.sql
@@ -1,7 +1,7 @@
-INSERT INTO
-    roles (id, role_name)
-VALUES
-    (1, 'admin'),
-    (2, 'university'),
-    (3, 'student'),
-    (4, 'company');
+-- INSERT INTO
+--     roles (id, role_name)
+-- VALUES
+--     (1, 'admin'),
+--     (2, 'university'),
+--     (3, 'student'),
+--     (4, 'company');
