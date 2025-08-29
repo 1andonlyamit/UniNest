93c10d7612b373156675c5fc315438ce5fc560fe
Rehan-Sakiwala <rehansakiwala@gmail.com>
2025-08-29 17:47:52 +0530

    Added password column

--- Backend/sql/01-roleType-roles.sql
+++ Backend/sql/01-roleType-roles.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(100) NOT NULL UNIQUE
 );
---Insertion of values
-INSERT INTO roles (id, role_name) VALUES
-    (1, 'admin'),
-    (2, 'university'),
-    (3, 'student'),
-    (4, 'company');
+
+INSERT INTO roles (role_name) VALUES
+    ('admin'),
+    ('university'),
+    ('student'),
+    ('company');
